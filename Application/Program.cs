d58fbebe19818ae4b02999c18053ae67437f728e
Zoubida Charif Khalifi <92330073+zoubida123ck@users.noreply.github.com>
2024-10-25 11:07:40 +0400

    Update comments

--- Application/Program.cs
+++ Application/Program.cs
@@ -8,6 +8,7 @@ namespace Application
 
     public class Program
     {
+        // The main method is the starting point of the program and manages the flow of the application.
         static void Main(string[] args)
         {
             CreateErrors(); // Run this whenever you want to generate a new set of data with errors
@@ -18,11 +19,10 @@ namespace Application
         {
             Data.CreateErrorData(Data.Objective, Data.ObjectiveError); // Create data with objective errors
             Data.CreateErrorData(Data.Subjective, Data.SubjectiveError); // Create data with subjective errors
-            Data.CreateErrorData(Data.Mixed, Data.MixedError); // Create data with with a mix of both subjective and objective errors
+            Data.CreateErrorData(Data.Mixed, Data.MixedError); // Create data with a mix of both subjective and objective errors
         }
-
-        private static void CreateProviders() // Create multiple providers with different error rates
-        {
+        // This method generates multiple datasets with varying error rates and percentages, which will be utilized to rank the resulting datasets according to their accuracy using the least squares distance method.
+        private static void CreateProviders() 
             Data.ERRORPERCENTAGE = 0.06;
             Data.ERRORRATE = 0.10;
             Data.CreateErrorData(Data.Provider1, Data.SubjectiveError);
